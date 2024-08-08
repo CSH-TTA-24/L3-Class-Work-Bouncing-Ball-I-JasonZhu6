f5e700419ecb0593c4972704cc46bd04e672561c
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 09:59:16 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let xPos = 25;
+let direction = 1
+
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+}
+
+function draw() {
+  background(0);
+
+  fill(0, 255, 0);
+  ellipse(xPos, 250, 50, 50);
+  
+  xPos += 10* direction;
+
+  if (xPos > 475 || xPos < 25) {
+    direction *= -1
+    
+    fill(0, 255, 0);
+    ellipse(xPos, 250, 50, 50);
+  } 
+}
